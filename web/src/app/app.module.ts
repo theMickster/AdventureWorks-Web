12aeb542508f7d2b5941e7639fee15a8083f2600
Mick Letofsky <mick.tosk@gmail.com>
2023-04-03 06:01:24 -0600

    First round of bootstrap theming

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { PlaygroundComponent } from './playground/playground.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaygroundComponent
   ],
   imports: [
     BrowserModule,
