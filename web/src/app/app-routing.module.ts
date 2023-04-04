b1761cd9954e60f61708e94079287d855ac10e0c
Mick Letofsky <mickl@knowledgeservices.com>
2023-04-04 16:38:57 -0600

    Adding 404 page not found component

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PlaygroundComponent } from './playground/playground.component';
+import { PageNotFoundComponent } from './public/oops/page-not-found.component';
 
 const routes: Routes = [
-  {path: 'playground', component: PlaygroundComponent}
+  {
+    path: 'playground',
+    component: PlaygroundComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
 ];
 
 @NgModule({
---
+++ web/src/app/public/public.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PageNotFoundComponent } from './oops/page-not-found.component';
+
+@NgModule({
+  declarations: [
+    PageNotFoundComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PublicModule { }
