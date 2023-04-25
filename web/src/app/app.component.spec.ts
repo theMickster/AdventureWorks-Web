6795a31cee84fca437fb2da991674e6397141193
Mick Letofsky <mickl@knowledgeservices.com>
2023-04-25 12:00:29 -0600

    Fixing broken unit tests

--- web/src/app/app.component.spec.ts
+++ web/src/app/app.component.spec.ts
@@ -1,15 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './public/footer/footer.component';
+import { PublicModule } from './public/public.module';
+import { HeaderComponent } from './public/header/header.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        PublicModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent,
+        HeaderComponent,
+        FooterComponent
       ],
     }).compileComponents();
   });
@@ -26,10 +34,18 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('Adventure Works Cycling');
   });
 
-  it('should render title', () => {
+  it('should render app-header', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome to Adventure Works Cycling!');
+    expect(compiled.querySelector('app-header')).not.toBe(null);
   });
+
+  it('should render app-footer', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('app-footer')).not.toBe(null);
+  });
+
 });
