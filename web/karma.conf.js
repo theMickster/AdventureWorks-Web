9094f81bea8355605dcfafabb069255916dfdfaf
Mick Letofsky <mickl@knowledgeservices.com>
2023-04-25 12:56:09 -0600

    Round One of Karma updates

--- web/karma.conf.js
+++ web/karma.conf.js
@@ -1,19 +1,18 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-process.env.CHROME_BIN = require('puppeteer').executablePath();
+process.env.CHROME_BIN = require("puppeteer").executablePath();
 
 module.exports = function (config) {
-
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-junit-reporter')
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage"),
+      require("@angular-devkit/build-angular/plugins/karma"),
+      require("karma-junit-reporter"),
     ],
     client: {
       captureConsole: true,
@@ -23,27 +22,38 @@ module.exports = function (config) {
         // for example, you can disable the random execution with `random: false`
         // or set a specific seed with `seed: 4321`
       },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage'),
-      subdir: '.',
+      dir: require("path").join(__dirname, "./coverage"),
+      subdir: ".",
       reporters: [
-        { type: 'html' },
-        { type: 'text-summary' },
-        { type: 'lcov' },
-        { type: 'cobertura' }
-      ]
+        { type: "html" },
+        { type: "text-summary" },
+        { type: "lcov" },
+        { type: "cobertura" },
+      ],
     },
-    reporters: ['progress', 'kjhtml', 'junit'],
+    junitReporter: {
+      outputDir: "/testresults/junit",
+      outputFile: "adventureWorksWeb_unitTestResults.xml",
+      useBrowserName: true,
+    },
+    coverageReporter: {
+      type: "cobertura",
+      dir: "testresults",
+      subdir: "coverage",
+      file: "adventureWorksWeb_unitTestCodeCoverage.xml",
+    },
+    reporters: ["progress", "kjhtml", "coverage", "junit"],
     port: 9877,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ["Chrome"],
     singleRun: false,
     restartOnFileChange: true,
     captureTimeout: 60000,
